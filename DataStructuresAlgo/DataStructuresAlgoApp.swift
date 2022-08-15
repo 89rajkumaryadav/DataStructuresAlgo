7efe07fd309ac539d499b81eb3857c3800d4bc43
rajkumaryadav89 <rajkumar.yadav@ecolane.com>
2022-08-15 17:26:23 +0530

    Initial Commit

---
+++ DataStructuresAlgo/DataStructuresAlgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresAlgoApp.swift
+//  DataStructuresAlgo
+//
+//  Created by Rajkumar Yadav on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresAlgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
